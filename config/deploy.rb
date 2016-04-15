9ffa3a4f908791199f439211da9ed3aea6f36326
Jerome Marhic <jerome.marhic@prium-solutions.com>
2016-04-15 11:15:16 +0200

    symlink to database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,9 +58,10 @@ namespace :deploy do
   task :symlink_config_files do
     run "#{ try sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
+     
 end
 
-
+after "deploy", "deploy:symlink_config_files" 
 
 
 
