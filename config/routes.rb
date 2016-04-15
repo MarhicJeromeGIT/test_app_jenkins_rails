86cb8af08afa68fec9108e109f6369f765dc9a4d
Jerome Marhic <jerome.marhic@prium-solutions.com>
2016-04-15 13:31:38 +0200

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'todos#index'
+  
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
